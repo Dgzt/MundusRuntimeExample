b4ebcce0a7a0be896c5d6ac575e1474c10bacdb1
Zsuró Tibor <zsurotibor@gmail.com>
2025-05-26 13:00:34 +0200

    Load custom fragment shader

--- core/src/com/mygdx/game/MundusExample.java
+++ core/src/com/mygdx/game/MundusExample.java
@@ -17,8 +17,11 @@ import com.mbrlabs.mundus.commons.Scene;
 import com.mbrlabs.mundus.commons.assets.SkyboxAsset;
 import com.mbrlabs.mundus.commons.assets.meta.MetaFileParseException;
 import com.mbrlabs.mundus.commons.utils.LightUtils;
+import com.mbrlabs.mundus.commons.utils.MundusShaderParser;
+import com.mbrlabs.mundus.commons.utils.ShaderUtils;
 import com.mbrlabs.mundus.runtime.Mundus;
 import net.mgsx.gltf.scene3d.attributes.FogAttribute;
+import net.mgsx.gltf.scene3d.shaders.PBRShaderConfig;
 
 import static com.badlogic.gdx.Application.LOG_INFO;
 
@@ -157,7 +160,10 @@ public class MundusExample extends ApplicationAdapter {
 
 		if (mundus.continueLoading()) {
 			// Loading complete, load a scene.
-			scene = mundus.loadScene("Main Scene.mundus");
+			PBRShaderConfig config = ShaderUtils.buildPBRShaderConfig(mundus.getAssetManager().maxNumBones);
+			config.fragmentShader = MundusShaderParser.parse(Gdx.files.internal("shaders/pbr.fs.glsl"));
+
+			scene = mundus.loadScene("Main Scene.mundus", config);
 
 			scene.cam.position.set(0, 40, 0);
 
