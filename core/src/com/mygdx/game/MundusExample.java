bfc18409c37880269eb15bc61c534a202f920a66
Zsuró Tibor <zsurotibor@gmail.com>
2025-05-28 08:29:39 +0200

    Draw a line and a pixel onto terrain

--- core/src/com/mygdx/game/MundusExample.java
+++ core/src/com/mygdx/game/MundusExample.java
@@ -207,14 +207,12 @@ public class MundusExample extends ApplicationAdapter {
 		final Terrain terrain = terrainComponent.getTerrainAsset().getTerrain();
 
 		final Pixmap pixmap = new Pixmap(512, 512, Pixmap.Format.RGBA8888);
-//		pixmap.setColor(Color.GOLDENROD);
 		pixmap.setColor(new Color(0f, 0f, 0f, 0f));
 		pixmap.fill();
 
 		pixmap.setColor(Color.BLACK);
-//		pixmap.drawLine(100, 100, terrain.terrainWidth, terrain.terrainDepth);
-//		pixmap.drawCircle(20, 20, 5);
-		pixmap.fillRectangle(10, 10, 492, 492); // ?
+		pixmap.drawPixel(1, 1);
+		pixmap.drawLine(10, 10, 40, 40);
 
 		final Texture texture = new Texture(pixmap);
 		// terrain.getMaterial().set(LinesTerrainMaterialAttribute.createLinesTerrainMaterialAttribute(texture));
