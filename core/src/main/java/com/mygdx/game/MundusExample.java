0f3c1e812027352ffe8bb46728b514ff089dc9fc
Zsuró Tibor <zsurotibor@gmail.com>
2025-07-24 05:14:16 +0200

    Update jolt physics system

--- core/src/main/java/com/mygdx/game/MundusExample.java
+++ core/src/main/java/com/mygdx/game/MundusExample.java
@@ -115,6 +115,14 @@ public class MundusExample extends ApplicationAdapter {
                 gameState = GameState.PLAYING;
                 break;
 			case PLAYING:
+                // Don't go below 30 Hz to prevent spiral of death
+                float deltaTime = (float)Math.min(Gdx.graphics.getDeltaTime(), 1.0 / 30.0);
+                if(deltaTime > 0) {
+                    // When running below 55 Hz, do 2 steps instead of 1
+                    final int numSteps = deltaTime > 1.0 / 55.0 ? 2 : 1;
+                    JoltPhysicsPlugin.update(deltaTime, numSteps);
+                }
+
 				play();
 				break;
 		}
